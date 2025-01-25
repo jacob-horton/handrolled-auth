4a6f8837c40e4c735743cded0d4d9bd6259dae4c
Jacob <hortonjacob1@gmail.com>
2025-01-25 14:59:57 +0000

    inital commit

---
+++ api/src/main.rs
@@ -0,0 +1,36 @@
+extern crate http_from_scratch;
+
+use http_from_scratch::{
+    request::Request,
+    response::{Response, Status},
+};
+
+use std::collections::HashMap;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    let req = Request::from_reader(&mut stream);
+
+    println!("{req:#?}");
+
+    let resp = Response {
+        version: "HTTP/1.1".to_string(),
+        status_code: Status::Ok,
+        headers: HashMap::new(),
+        body: req.body,
+    };
+
+    println!("{}", resp.to_string());
+
+    stream.write_all(resp.to_string().as_bytes()).unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
